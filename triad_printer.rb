34e6c4ccac8144ea9cb541aa2a6497f3b4c23e0a
miniengineer <samadova.minira@gmail.com>
2020-06-07 20:21:33 +0900

    added y coordinate converter

--- triad_printer.rb
+++ triad_printer.rb
@@ -32,16 +32,32 @@ class TriadPrinter
 ----------------"
   end
 
+  def convert_y_coordinate(y_coordinate)
+    case y_coordinate
+    when 0
+      return 2
+    when 1
+      return 6
+    when 2
+      return 10
+    when 3
+      return 14
+    end
+  end
+
   def print_triad(coordinates)
-    if coordinates[:r][1] == 14
+    if coordinates[:r][1] == 3
       result = @four_tread
     else
       result = @three_tread
     end
     result = result.split("\n")
-    result[coordinates[:r][1]][coordinates[:r][0] * 3] = "R"
-    result[coordinates[:t][1]][coordinates[:t][0] * 3] = "3"
-    result[coordinates[:f][1]][coordinates[:f][0] * 3] = "5"
+    r_y = convert_y_coordinate(coordinates[:r][1])
+    t_y = convert_y_coordinate(coordinates[:t][1])
+    f_y = convert_y_coordinate(coordinates[:f][1])
+    result[r_y][coordinates[:r][0] * 3] = "R"
+    result[t_y][coordinates[:t][0] * 3] = "3"
+    result[f_y][coordinates[:f][0] * 3] = "5"
     return result
   end
 end

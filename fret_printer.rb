07b497aab662eb1c73b879b1a3705ab5a2d99e9d
miniengineer <samadova.minira@gmail.com>
2020-06-12 23:22:22 +0900

    moved printing to a separate class

--- fret_printer.rb
+++ fret_printer.rb
@@ -19,4 +19,21 @@ class FretPrinter
     end
     fret + '----------------'
   end
+
+  def add_triad(gt)
+    case gt.triad.position
+    when Triad::ROOT_POSITION
+      add_note string, gt.triad.root
+      add_note string + 1, gt.triad.third
+      add_note string + 2, gt.triad.fifth
+    when Triad::FIRST_INVERSION
+      add_note string, gt.triad.third
+      add_note string + 1, gt.triad.fifth
+      add_note string + 2, gt.triad.root
+    when Triad::SECOND_INVERSION
+      add_note string, gt.triad.fifth
+      add_note string + 1, gt.triad.root
+      add_note string + 2, gt.triad.third
+    end
+  end
 end

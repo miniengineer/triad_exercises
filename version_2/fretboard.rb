58107dc17c338a512a27e00105c765af115b5ccd
miniengineer <samadova.minira@gmail.com>
2020-06-13 15:16:37 +0900

    implement Fretboard class

--- version_2/fretboard.rb
+++ version_2/fretboard.rb
@@ -1,6 +1,25 @@
-class Frethboard
+require_relative 'scale'
+
+class Fretboard
+  attr_reader :tuning, :strings, :played_frets
 
   def initialize
+    # highest string to lowest string
+    @tuning = %w[E B G D A E]
+    @strings = tuning.map { |note| Scale.chromatic(note) }
+    @played_frets = Array.new(tuning.length, nil)
+  end
 
+  def play_note(string_num, note)
+    play_fret(string_num, find_fret(string_num, note))
+  end
+
+  def find_fret(string_num, note)
+    played_string_notes = strings[string_num]
+    played_string_notes.index note
+  end
+
+  def play_fret(string_num, fret_num)
+    played_frets[string_num] = fret_num
   end
 end

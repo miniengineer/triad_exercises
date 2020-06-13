11775792c6f821c89213251ebeb0cd9fc4699227
miniengineer <samadova.minira@gmail.com>
2020-06-13 13:20:03 +0900

    added space after magic comment

---
+++ version_2/guitar_triad.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+class GuitarTriad
+  attr_reader :top_string
+
+  def initialize(triad, top_string)
+    @triad = triad
+    @top_string = top_string
+  end
+
+  def middle_string
+    # generate middle string
+  end
+
+  def last_string
+    # generate last string
+  end
+
+  def min_note_fret
+
+  end
+
+  def max_note_fret
+
+  end
+end
--- version_2/note.rb
+++ version_2/note.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Note
   CHROMATIC_SCALE = %w[A A# B C C# D D# E F F# G G#]
 end

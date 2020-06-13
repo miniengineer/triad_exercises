50c404afb00e9a362cbd0967ce6cbe3dc2600c27
miniengineer <samadova.minira@gmail.com>
2020-06-13 13:55:44 +0900

    refactored note to scale class

--- version_2/note.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-class Note
-  CHROMATIC_SCALE = %w[A A# B C C# D D# E F F# G G#]
-end
---
+++ version_2/scale.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class Scale
+  CHROMATIC = %w[A A# B C C# D D# E F F# G G#]
+end
